610c67cdfa0a91219e0863d888d0d39044fe4990
praveen kumar <praveenkumar.tst@gmail.com>
2023-04-19 00:09:48 +0530

    Mongodb shell script

--- frontend.sh
+++ frontend.sh
@@ -1,9 +1,22 @@
+echo -e \e[32m>>>>>>>>>>>> install nginx <<<<<<<<<<<\e[0m]
 yum install nginx -y
+
+echo -e \e[32m>>>>>>>>>>>> remove nginx content <<<<<<<<<<<\e[0m]
 rm -rf /usr/share/nginx/html/*
+
+echo -e \e[32m>>>>>>>>>>>> install frontend content <<<<<<<<<<<\e[0m]
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+
+echo -e \e[32m>>>>>>>>>>>> goto the removed nginx content location<<<<<<<<<<<\e[0m]
 cd /usr/share/nginx/html
+
+echo -e \e[32m>>>>>>>>>>>> unzip frontend content <<<<<<<<<<<\e[0m]
 unzip /tmp/frontend.zip
+
+echo -e \e[32m>>>>>>>>>>>> copy configuration file <<<<<<<<<<<\e[0m]
 cp roboshop.conf /etc/nginx/default.d/roboshop.conf
+
+echo -e \e[32m>>>>>>>>>>>> enable and start nginx <<<<<<<<<<<\e[0m]
 systemctl enable nginx
 systemctl start nginx
 
