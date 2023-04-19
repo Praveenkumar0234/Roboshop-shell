a68432f8df4c7af722650dae56f00f0a8c20ac50
praveen kumar <praveenkumar.tst@gmail.com>
2023-04-19 22:43:54 +0530

    user shell script changes

---
+++ cart.sh
@@ -0,0 +1,39 @@
+echo -e "\e[33m>>>>>>>>>>>> configure nodejs repo <<<<<<<<\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[33m>>>>>>>>>>>> install nodejs <<<<<<<<\e[0m"
+yum install nodejs -y
+
+echo -e "\e[33m>>>>>>>>>>>> Add application user <<<<<<<<\e[0m"
+useradd roboshop
+
+echo -e "\e[33m>>>>>>>>>>>> crate directory <<<<<<<<\e[0m"
+rm -rf /app
+mkdir /app
+
+echo -e "\e[33m>>>>>>>>>>>> download Application code  <<<<<<<<\e[0m"
+curl -L -o /tmp/cart.zip https://roboshop-artifacts.s3.amazonaws.com/cart.zip
+
+echo -e "\e[33m>>>>>>>>>>>> change directory to the app <<<<<<<<\e[0m"
+cd /app
+
+
+echo -e "\e[33m>>>>>>>>>>>> Unzip the application code <<<<<<<<\e[0m"
+unzip /tmp/cart.zip
+
+
+echo -e "\e[33m>>>>>>>>>>>> install nodejs dependency <<<<<<<<\e[0m"
+npm install
+
+echo -e "\e[33m>>>>>>>>>>>> copy the cart service <<<<<<<<\e[0m"
+cp /root/Roboshop-shell/cart.service /etc/systemd/system/cart.service
+
+echo -e "\e[33m>>>>>>>>>>>> reload the service <<<<<<<<\e[0m"
+systemctl daemon-reload
+
+echo -e "\e[33m>>>>>>>>>>>> enable and start the service <<<<<<<<\e[0m"
+systemctl enable cart
+systemctl start cart
+
+
+
