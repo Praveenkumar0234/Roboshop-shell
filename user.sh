aa07c4a02a2aa46262397e21d92946bd77c05b53
praveen kumar <praveenkumar.tst@gmail.com>
2023-04-21 15:08:50 +0530

    shell script changes

--- user.sh
+++ user.sh
@@ -1,8 +1,6 @@
-script_path=${dirname $0}
+script_path=$(dirname $0)
 source ${script_path}/common.sh
 
-
-exit
 
 echo -e "\e[33m>>>>>>>>>>>> configure nodejs repo <<<<<<<<\e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash
